971280dfff159d06c63568da8d84d1799c5aad86
Anders Renström <anders.renstroom@gmail.com>
2021-03-03 11:25:33 +0100

    test:(Implemented tests for 1-sparse recovery algorithm) #6

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -1,5 +1,6 @@
-from algorithms.streaming.misra_gries import (
+from algorithms.streaming import (
     misras_gries,
+    one_sparse
 )
 import unittest
 
@@ -14,3 +15,14 @@ class TestMisraGreis(unittest.TestCase):
         self.assertEqual(None,misras_gries([0,0,0,2,1,1,1]))
         self.assertEqual(None,misras_gries([0,0,0,1,1,1]))
     
+
+class TestOneSparse(unittest.TestCase):
+    def test_one_sparse_correct(self):
+        self.assertEqual(4,one_sparse([(4,'+'), (2,'+'),(2,'-'),(4,'+'),(3,'+'),(3,'-')]))
+        self.assertEqual(2,one_sparse([(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+')]))
+
+
+    def test_one_sparse_incorrect(self):
+        self.assertEqual(None,one_sparse([(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(1,'+')])) #Two values remaining
+        self.assertEqual(None,one_sparse([(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'-'),(2,'-'),(2,'-'),(2,'-')])) # No values remaining
+        self.assertEqual(None,one_sparse([(2,'+'),(2,'+'),(4,'+'),(4,'+')])) # Bitsum sum of sign is inccorect
